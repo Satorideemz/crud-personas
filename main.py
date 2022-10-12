4c28f430e1071a987164e9b92fe7086d3b9f3812
Satori <bl00dztr34m@gmail.com>
2022-10-12 08:03:50 -0300

    separo el programa del test

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import unittest
-
 class Persona:
     def __init__(self,nombre,apellido,dni):
         self.nombre=nombre 
@@ -41,50 +39,3 @@ class Personaservice:
         for i in range(len(self.personas)):
             file.write("{},{},{}, \n".format(self.personas[i].nombre, self.personas[i].apellido, self.personas[i].dni))
         file.close()
-
-
-class TestApp(unittest.TestCase):
-    
-    def test_add(self):
-        repo1=Personaservice()
-        persona1=Persona("pepe","honguito",123)
-        repo1.add(persona1)
-        self.assertDictEqual(repo1.__dict__,{'personas': [persona1] })
-        
-    def test_delete(self):
-        repo1=Personaservice()
-        persona1=Persona("fulano","mengano",864)
-        repo1.add(persona1)
-        repo1.delete(persona1)
-        self.assertDictEqual(repo1.__dict__,{'personas': [] })
-
-    def test_update(self):
-        repo1=Personaservice()
-        persona1=Persona("cualquier","cosa",000)
-        persona2=Persona("alberto","cortez",85)
-        repo1.add(persona1)
-        repo1.update(persona1,persona2)
-        self.assertDictEqual(repo1.__dict__,{'personas': [persona2] })
-
-    def test_findbydocumento_sucess(self):
-        repo1=Personaservice()
-        persona1=Persona("huang","lee", 32467)
-        repo1.add(persona1)
-        self.assertEqual(persona1,repo1.findByDocumento(32467))
-
-    def test_findbydocumento_failed(self):
-        repo1=Personaservice()
-        persona1=Persona("huang","lee", 32467)
-        repo1.add(persona1)
-        self.assertEqual(None,repo1.findByDocumento(00000))
-
-    def test_findAll(self):
-        repo1=Personaservice()
-        persona1=Persona("frederik","weber", 8976)
-        persona2=Persona("kate","denson", 5681)
-        repo1.add(persona1)
-        repo1.add(persona2)
-        self.assertDictEqual(repo1.__dict__,{'personas': [persona1,persona2] })
-
-if __name__ == '__main__':
-    unittest.main()
